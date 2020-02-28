d306a71b19800841ac27b9c488323b23581d43f4
ITDev Star <itdevstar@hotmail.com>
2020-02-28 18:37:05 +0200

    play by avplayer

--- StreamPlayer/StreamPlayer/ViewController.swift
+++ StreamPlayer/StreamPlayer/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+  //  private var playerLayer: AVPlayerLayer!
+   // private var player: AVPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        playStartVideo()
     }
 
+    private func playStartVideo() {
+        
+        let fileURL = NSURL(string: "http://www.aaa.com/m3u8/l.m3u8")
+        
+        let avAsset = AVURLAsset(url: fileURL! as URL, options: nil)
+       
+        let item = AVPlayerItem(asset: avAsset)
+               
+        
+        let player = AVPlayer(playerItem: item)
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.view.bounds
+        self.view.layer.addSublayer(playerLayer)
+        player.play()
+    }
 
 }
 
